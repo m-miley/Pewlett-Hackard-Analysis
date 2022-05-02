67f68546e82773f2a709a87eefc2d4b44082d4b2
markm3690 <mrmileyy@gmail.com>
2022-05-02 16:19:03 -0500

    update ERD/schema

--- schema.sql
+++ schema.sql
@@ -39,9 +39,9 @@ CREATE TABLE dept_emp (
 	emp_no INT NOT Null,
 	dept_no VARCHAR(4) NOT Null,
 	from_date DATE NOT Null,
-	to_date DATE NOT Null
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+	to_date DATE NOT Null,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 
@@ -50,8 +50,8 @@ CREATE TABLE titles (
 	title VARCHAR NOT Null,
 	from_date DATE NOT Null,
 	to_date DATE NOT Null,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
 SELECT * 
